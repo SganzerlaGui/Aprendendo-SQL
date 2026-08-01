1a1ad141b354b7e4d49479a73685d261064a19a8
Guilherme Sganzerla Correia <gui.sganzerla2007@gmail.com>
2026-08-01 09:52:39 -0300

    Update .sql

--- .sql
+++ .sql
@@ -4,3 +4,7 @@ SELECT * FROM tabelafornecedores WHERE pa_s_de_origem = 'China'
 -- O FROM serve para selecionar a tabela que você deseja buscar as informações
 -- O WHERE  serve para filtrar as linhas da tabela 
 -- Tudo isso que  falei é sobre o codigo acima! 
+
+SELECT DISTINCT cliente FROM tabelapedidos;
+
+-- O DISTINCT que utilizei logo acima serve para você selecionar uma informação, como o nome diz, distinta.  ----> Assim, vindo logo após o SELECT
