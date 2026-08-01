21b7826680197448fa27856fc000f88370a2209b
Guilherme Sganzerla Correia <gui.sganzerla2007@gmail.com>
2026-08-01 09:36:04 -0300

    Create .sql

---
+++ .sql
@@ -0,0 +1,6 @@
+SELECT * FROM tabelafornecedores WHERE pa_s_de_origem = 'China'
+
+-- O SELECT serve para selecionar algo de um banco de dados -----> Neste caso o * está selecionando tudo, todas as informações
+-- O FROM serve para selecionar a tabela que você deseja buscar as informações
+-- O WHERE  serve para filtrar as linhas da tabela 
+-- Tudo isso que  falei é sobre o codigo acima! 
